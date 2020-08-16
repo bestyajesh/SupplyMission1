6c549154f9ccdea389244b7b8133eea0a9ca6dff
Besty <67360804+bestyajesh@users.noreply.github.com>
2020-08-16 16:39:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,7 +54,7 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
+    Matter.Body.setStatic(packageBody,false);
     
   }
 }
